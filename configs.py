dcb367658c1cb7d972724ceb0e733664254b7624
SAHILSING8901 <sabuji8901@gmail.com>
2024-08-20 12:05:58 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,7 +11,7 @@ class Config(object):
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "5000247903"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Buwanabot:<sabuji@8901>@atlascluster.fncm4kc.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002065165415"))
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002165310608"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
