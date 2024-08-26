c99bcdd149b297a062f67bc0a964715779294091
sahilkalkandha7058 <sabuji8902@gmail.com>
2024-08-26 07:23:20 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -36,7 +36,7 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
  
  I am Super noob Please Support My Hard Work.
 
-[Donate Me](https://t.me/KingVj01)
+[Donate Me](1)
 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
