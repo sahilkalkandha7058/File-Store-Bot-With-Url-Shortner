7418febcc8b58581d1d65c82d04e193dd8117944
Rajps44 <singhayush88805@gmail.com>
2024-08-26 12:37:48 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -59,8 +59,8 @@ async def handle_force_sub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥](https://t.me/VJ_Bot_Disscussion).",
-            disable_web_page_preview=True
+            text="Something went Wrong. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥]().",
+            disable_web_page_preview=False
         )
         return 200
     return 200
